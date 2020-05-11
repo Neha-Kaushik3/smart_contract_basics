1b92b87cfde6aac34b54922801a25fc3f4f4c81b
Neha-Kaushik3 <61658106+Neha-Kaushik3@users.noreply.github.com>
2020-05-11 21:52:08 +0530

    Update SimpleTransferAndWithdraw.sol

--- SimpleTransferAndWithdraw.sol
+++ SimpleTransferAndWithdraw.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.5.11 <0.7.0;
 
 contract SendMonenyExample{
     uint public balanced_recieved;
-    
+    //payable is keyword that enable the address/variable in order to recieve money
     function receive_money () public payable{                   //This function will store money in the variable balanced_recieved
         balanced_recieved+=msg.value;
     }
@@ -11,8 +11,13 @@ contract SendMonenyExample{
         return address(this).balance;
     }
     
+    //function to transfer money to the one who is calling the function
     function Withdarw_money() public {
         address payable _to = msg.sender;
         _to.transfer(this.get_balance());
     }
-}
+    //function to transfer to a arbitary address
+    function WithdrawMoneyTO (address payable to) public {
+        to.transfer(this.get_balance());
+    }
+} 
